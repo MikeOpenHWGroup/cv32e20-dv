1d4a03a2cefea8682418b5af3ff2b709f248cb6f
karabambus <mradic07@gmail.com>
2026-05-05 16:44:05 +0200

    bumped hash to latest version

--- sim/ExternalRepos.mk
+++ sim/ExternalRepos.mk
@@ -40,4 +40,5 @@ SVLIB_HASH       ?= c25509a7e54a880fe8f58f3daa2f891d6ecf6428
 # ACT4 (RISC-V Architectural Certification Tests)
 ACT4_REPO   ?= https://github.com/riscv-non-isa/riscv-arch-test
 ACT4_BRANCH ?= act4
-ACT4_HASH   ?= 362b819b97087e96dbe4a04cce70c1515c21472b
+ACT4_HASH   ?= cf347755f910a6f717b73665823de0c98dd6b53e
+
